bdf76c3999a26496d9a492181db5d687b9276a39
Lebogang <lebogangodeveloper@gmail.com>
2024-01-30 15:30:50 +0200

    Implement FIFO caching policy
    - Create FIFOCache class that inherits from BaseCaching.
    - This caching system uses the FIFO (First-In-First-Out) algorithm
    to manage items in the cache.
    - The put method adds an item to the cache and,
    if the number of items exceeds BaseCaching.MAX_ITEMS,
    it discards the first item added (FIFO).
    - The get method retrieves the value linked to the key in cache_data.
    If key or item is None, the put method does nothing.

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+FIFOCache class module that inherits from BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+        self.current_keys = []	
+
+    def put(self, key, item):
+        """
+        Add an item to the cache (FIFO algorithm)
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                discarded_key = self.current_keys.pop(0)
+                del self.cache_data[discarded_key]
+                print('DISCARD: {}'.format(discarded_key))
+            if key not in self.current_keys:
+                self.current_keys.append(key)
+
+    def get(self, key):
+        """
+        Get an item from the cache
+        """
+        return self.cache_data.get(key, None)
