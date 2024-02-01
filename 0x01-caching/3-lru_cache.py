7eb1fe2bf81fdc21ac93424d29bda37024507b5c
Lebogang <lebogangodeveloper@gmail.com>
2024-02-01 09:19:03 +0200

    fix: LRUCache implementation
    - Addressed an issue in the LRUCache class,
    - where the put and get methods conditions,
    were not correctly handling the LRU cache eviction policy

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -23,22 +23,21 @@ class LRUCache(BaseCaching):
         Add an item to the cache using LRU algorithm
         """
         if key is not None and item is not None:
-            self.cache_data[key] = item
-
             if key in self.order_used:
                 self.order_used.remove(key)
-            self.order_used.insert(0, key)
-
-            if len(self.order_used) > BaseCaching.MAX_ITEMS:
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 discarded_key = self.order_used.pop(-1)
                 del self.cache_data[discarded_key]
                 print('DISCARD: {}'.format(discarded_key))
+
+            self.cache_data[key] = item
+            self.order_used.insert(0, key)
 
     def get(self, key):
         """
         Get an item from the cache
         """
-        if key in self.order_used:
+        if key in self.cache_data:
             self.order_used.remove(key)
-            self.order_used.append(key)
+            self.order_used.insert(0, key)
         return self.cache_data.get(key, None)
