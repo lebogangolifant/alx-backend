5c909a2988240eff6910ac16c1818e5487e33ec7
Lebogang <lebogangodeveloper@gmail.com>
2024-02-06 11:18:55 +0000

    Implement internationalization for templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Flask app with get_locale function using request.accept_languages.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+from typing import List
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determines the best match for the user's preferred language.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    Renders the index.html template.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
